f4bd2a34dca90e9f7fe2bccb4aa7559ec64226f9
halogenandtoast <halogenandtoast@gmail.com>
2014-07-03 11:58:29 -0400

    Added sign up page
    * ran: rails g model user email
    * Added sign up view
    * Added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "galleries#index"
+  resources :users, only: [:new]
   resources :galleries do
     resources :images, only: [:new, :create, :show, :edit, :update, :destroy]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702151200) do
+ActiveRecord::Schema.define(version: 20140703155659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,11 @@ ActiveRecord::Schema.define(version: 20140702151200) do
   end
 
   add_index "images", ["gallery_id"], name: "index_images_on_gallery_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
