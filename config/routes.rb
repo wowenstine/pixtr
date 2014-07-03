0f6aaee9ad63093bdc4ab9b0816551dc75cd13e9
halogenandtoast <halogenandtoast@gmail.com>
2014-07-03 13:48:18 -0400

    User sign up
    * Added presence validation on user email
    * Added users#create
    * Show errors on users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "galleries#index"
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
   resources :galleries do
     resources :images, only: [:new, :create, :show, :edit, :update, :destroy]
   end
