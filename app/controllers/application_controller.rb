a53d7f7c9e2ef3f68259665fae7c662962fd262d
halogenandtoast <halogenandtoast@gmail.com>
2014-07-04 11:12:33 -0400

    Implement current_user
    * Added current_user helper_method
    * Added signed_in? helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    if cookies[:user_id]
+      @user ||= User.find_by(id: cookies.signed[:user_id])
+    end
+  end
+  helper_method :current_user
+
+  def signed_in?
+    current_user
+  end
+  helper_method :signed_in?
+
 end
